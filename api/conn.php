e1d8e8f0df9c07830a538bb95803bdae08c86f66
0x4572696368 <erichjordyec@gmail.com>
2021-06-25 13:17:25 -0500

    upload

--- api/conn.php
+++ api/conn.php
@@ -4,11 +4,6 @@ $username = "root";
 $password = "";
 $dbname = "so_store";
 
-// $servername = "localhost";
-// $username = "id16698738_mrhackadmin";
-// $password = "Mr.Hack595@/";
-// $dbname = "id16698738_mrhack";
-
 // try {
 //   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
 //   // set the PDO error mode to exception
